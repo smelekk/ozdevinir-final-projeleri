054eef0398e8f8c2e0d1a8b68fd0c17840c2a1f4
Sude Melek Çelik <sudemelekcelik@gmail.com>
2026-05-23 02:57:29 +0300

    Refactor Turing machine for license plate recognition

--- odev2-plaka-tanima.py
+++ odev2-plaka-tanima.py
@@ -1,105 +1,93 @@
 class TuringMakinesi:
-    def __init__(self, girdi):
-        self.girdi_orjinal = girdi
-        self.bant = list(girdi) + ['B']  #bant sonuna boşluk ekleniyor
+    def __init__(self, plaka):
+        # Bant yapısı: Girdi sonuna boşluk sembolü ekliyoruz (Örn: '_')
+        self.bant = list(plaka) + ['B']
         self.kafa = 0
+
+        # Durumlar (Sizin görselinizdeki adlandırmalara uygun)
         self.durum = 'q0'
-        self.kabul_durumu = 'qKABUL'
-        self.red_durumu = 'qRED'
+        self.kabul = 'qKABUL'
+        self.red = 'qRED'   # qRED durumu 
+        self.adim = 1
 
-        self.gecis_fonksiyonu = {
-            'q0': ('rakam', 'q1'),
-            'q1': ('rakam', 'q2'),
-            'q2': ('harf',  'q3'),
-            'q3': ('harf',  'q4'),
-            'q4': ('rakam', 'q5'),
-            'q5': ('rakam', 'q6'),
-            'q6': ('rakam', 'q7')
-        }
-
+        # Alfabeler
         self.rakamlar = "0123456789"
         self.harfler = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        self.sonuc = None
 
-    def karakter_turu(self, karakter):
-        if karakter in self.rakamlar:
-            return 'rakam'
-        elif karakter in self.harfler:
-            return 'harf'
-        elif karakter == 'B':
-            return 'bosluk'
-        else:
-            return 'gecersiz'
+        # Geçiş fonksiyonunu oluştur
+        self.gecisler = self.gecis_fonk()
 
-    # Bant görüntüleme
-
-    def bandi_yazdir(self):
-        bant_gorunumu = ""
-        for i, karakter in enumerate(self.bant):
-
-            if i == self.kafa:
-                bant_gorunumu += f"[{karakter}] "
-            else:
-                bant_gorunumu += f" {karakter}  "
-        return bant_gorunumu
+    def gecis_fonk(self):
+        gecis = {}
+        for r in self.rakamlar:
+            gecis[('q0', r)] = ('q1', r, 'R') #ilk rakam
+        for r in self.rakamlar:
+            gecis[('q1', r)] = ('q2', r, 'R') #ikinci rakam
+        for h in self.harfler:
+            gecis[('q2', h)] = ('q3', h, 'R') #birinci harf
+        for h in self.harfler:
+            gecis[('q3', h)] = ('q4', h, 'R') #ikinci harf
+        for r in self.rakamlar:
+            gecis[('q4', r)] = ('q5', r, 'R') #üçüncü rakam
+        for r in self.rakamlar:
+            gecis[('q5', r)] = ('q6', r, 'R') #dördüncü rakam
+        for r in self.rakamlar:
+            gecis[('q6', r)] = ('q7', r, 'R') #beşinci rakam
+        gecis[('q7', 'B')] = (self.kabul, 'B', 'S')  #kabul durumu
+        return gecis
     
-    def adim_yazdir(self, okunan):
-        print(f"Durum: {self.durum}  Okunan Sembol: {okunan}")
-        print(f"Bant: {self.bandi_yazdir()}")
-        print("-" * 50)
+    def bant_yazdir(self, okunan, yazilan, yon):
+        bant_gorunum = "".join(self.bant)
+        bilgi = (
+            f"Adım: {self.adim} | "
+            f"Durum: {self.durum} | "
+            f"Okunan: {okunan} | "
+            f"Yazılan: {yazilan} | "
+            f"Hareket: {yon}"
+        )
+        print(bilgi)
+        print("Bant :", bant_gorunum)
+        print("       " + " " * self.kafa + "^")
 
-    # Turing Makinesi çalıştırma
     def calistir(self):
-        print("\n" + "=" * 50)
-        print("TURING MAKİNESİ PLAKA TANIYICI")
-        print("=" * 50)
-        print(f"Girdi: {self.girdi_orjinal}")
-        print()
+        print("TURING MAKİNESİ PLAKA TANIYICI (NNLLNNN)")
+        print("Girdi:", "".join(self.bant).replace("_", ""))
+        print("\n" + "-" * 60)
 
-        while self.sonuc is None:
+        while self.durum not in [self.kabul, self.red]:
+            # Bant sonuna çıkılırsa boşluk oku
             if self.kafa >= len(self.bant):
-                self.durum = self.red_durumu
-                self.sonuc = "RED"
-                break
-                
-            okunan = self.bant[self.kafa]
-            
-            self.adim_yazdir(okunan)
-            if self.durum == 'q7':
-                if okunan == 'B':  # Girdi tam 7 karakter ve bitti
-                    self.durum = self.kabul_durumu
-                    self.sonuc = "KABUL"
-                else:  
-                    self.durum = self.red_durumu
-                    self.sonuc = "RED"
-                break
-
-            # GEÇİŞ FONKSİYONU KONTROLÜ (q0 - q6 arası)
-            if self.durum in self.gecis_fonksiyonu:
-                beklenen_tur, sonraki_durum = self.gecis_fonksiyonu[self.durum]
-                guncel_tur = self.karakter_turu(okunan)
-                
-                if guncel_tur == beklenen_tur:
-                    self.durum = sonraki_durum
-                    self.kafa += 1  # Geçerli karakterde kafa sağa kayar (R)
-                else:
-                    # Beklenen dışında
-                    self.durum = self.red_durumu
-                    self.sonuc = "RED"
-                    break
+                okunan = 'B'
             else:
-                self.durum = self.red_durumu
-                self.sonuc = "RED"
-                break
+                okunan = self.bant[self.kafa]
 
-        # Sonuç çıktısı
-        print(f"SONUÇ: {self.sonuc}")
-        print("=" * 50)
-        return self.sonuc
-     
-# ANA PROGRAM
+            anahtar = (self.durum, okunan)
+
+            if anahtar in self.gecisler:
+                yeni_durum, yazilan, yon = self.gecisler[anahtar]
+                self.bant[self.kafa] = yazilan  #banda yazar
+                self.bant_yazdir(okunan, yazilan, yon)  #adımı yazdırır
+                self.durum = yeni_durum #durum güncellenir
+
+                if yon == 'R': #kafayı sağa hareket ettir
+                    self.kafa += 1
+                elif yon == 'L':
+                    self.kafa -= 1
+            else:
+                # qRED durumuna geçiş
+                self.durum = self.red
+                self.bant_yazdir(okunan, okunan, 'S')
+            self.adim += 1
+        if self.durum == self.kabul:
+            print("SONUÇ: KABUL")
+        else:
+            print("SONUÇ: RED")
+
 if __name__ == "__main__":
-    plaka = input("Plaka giriniz: ")
-    tm = TuringMakinesi(plaka)
-    tm.calistir()
-    print()
+    while True:
+        plaka = input("Plaka giriniz: ")
+        
+        tm = TuringMakinesi(plaka)
+        tm.calistir()
+        print("\n")
+        break
